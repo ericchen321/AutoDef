cac99885d761882697e4ae134ec8b14925254a8d
eric <cgx1997@outlook.com>
2021-11-19 00:53:20 -0800

    updated installation steps (upt to libigl)

--- installDependencies.sh
+++ installDependencies.sh
@@ -15,6 +15,12 @@ wget -O deps/bazel.sh https://github.com/bazelbuild/bazel/releases/download/0.10
 sudo bash deps/bazel.sh
 
 # build and install my version of tensorflow
+# remove the author's tensorflow_cc
+rm -rf extern/tensorflow_cc/
+# clone Eric's tensorflow_cc
+cd extern/
+git clone https://github.com/ericchen321/tensorflow_cc.git
+cd ..
 cd extern/tensorflow_cc/tensorflow_cc
 mkdir build
 cd build
@@ -23,6 +29,8 @@ make -j8 && sudo make install
 cd tensorflow
 ./bazel-bin/tensorflow/tools/pip_package/build_pip_package ./pip_package_build
 cd ../../../../../ # back to AutoDef/
+# upgrade pip3
+pip3 install --upgrade pip
 extern/anaconda/bin/pip install --upgrade extern/tensorflow_cc/tensorflow_cc/build/tensorflow/pip_package_build/tensorflow-1.8.0-cp36-cp36m-linux_x86_64.whl
 
 # Install Keras
@@ -34,15 +42,25 @@ cp ./keras.json ~/.keras/
 
 
 ### Now the rest of the related code
+# remove the author's GAUSS
+rm -rf extern/GAUSS/
+# clone Eric's GAUSS recursively
+cd extern/
+git clone --recursive https://github.com/ericchen321/GAUSS.git
+cd ..
 # Build GAUSS
 cd extern/GAUSS
 bash InstallGAUSS_Ubuntu_noqt.sh
 cd ../../
 
 # Build Libigl Python bindings
+rm -rf extern/libigl/
+git clone --recursive git@github.com:lawsonfulton/libigl-legacy.git extern/libigl
 cd extern/libigl/python
 mkdir build
-cd build; cmake ..; make -j8;
+cd build
+cmake ..
+make -j8
 cd ../../../../
 
 # Build Cubacode
